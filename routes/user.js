36983f612112a25f5627e8ee65606a4cf9d3c2cf
yunjeongCho1 <yjcho0907@gmail.com>
2023-08-30 20:17:48 +0900

    r

--- routes/user.js
+++ routes/user.js
@@ -112,7 +112,7 @@ router.post("/delete_account", authMiddleware, async (req, res) => {
       } else {
         //await Review.deleteMany({ user_id: user._id });
         const del_user = await User.findByIdAndDelete(user._id);
-        console.log("delete: ", del_user);
+        console.log("delete ", del_user);
         res.status(200).json(del_user);
       }
     }

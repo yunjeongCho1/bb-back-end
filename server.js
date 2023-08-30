f81d83812003fd44621d8e1f1ff9210e1e56fcf8
yunjeongCho1 <yjcho0907@gmail.com>
2023-08-30 15:51:47 +0900

    0830.ver

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const PORT = 8000;
 
 app.use(express.json());
 var cors = require("cors");
-app.use(cors());
+let corsOptions = {
+  origin: "*", // 출처 허용 옵션
+  credential: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+};
+
+app.use(cors(corsOptions));
+//app.use(cors());
 
 const jwt = require("jsonwebtoken");
 

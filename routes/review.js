d50420a37b2b400a88cf740282e36b7b5e9ba7f2
yunjeongCho1 <yjcho0907@gmail.com>
2023-08-30 19:32:36 +0900

    test

--- routes/review.js
+++ routes/review.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 const Review = require("../schema/reviewschema");
 const User = require("../schema/userschema");
-const cors = require("cors");
-router.options("/backup", cors());
+//const cors = require("cors");
+//router.options("/backup", cors());
 
 require("dotenv").config();
 var secret_key = process.env.SECRET_KEY;
@@ -152,32 +152,33 @@ router.put("/:id", authMiddleware, async (req, res) => {
 const fs = require("fs");
 const path = require("path");
 
-router.get("/backup", cors(), authMiddleware, async (req, res) => {
-  try {
-    const user = await User.findById(req.user._id);
+router.get("/backup", authMiddleware, async (req, res) => {
+  res.status(200).send("0000000000000000");
+  // try {
+  //   const user = await User.findById(req.user._id);
 
-    if (user) {
-      const reviews = await Review.find({ user_id: user._id });
+  //   if (user) {
+  //     const reviews = await Review.find({ user_id: user._id });
 
-      const backupData = {
-        user: user.email,
-        reviews: reviews,
-      };
+  //     const backupData = {
+  //       user: user.email,
+  //       reviews: reviews,
+  //     };
 
-      const backupFileName = `test1_backup.json`;
-      const backupFilePath = path.join(__dirname, "backups", backupFileName);
-      fs.writeFileSync(backupFilePath, JSON.stringify(backupData, null, 2));
-      res.download(backupFilePath, backupFileName, () => {
-        // 다운로드 후 백업 파일 삭제
-        fs.unlinkSync(backupFilePath);
-      });
-    } else {
-      res.status(500).json({ message: "유저를 찾을 수 없습니다." });
-    }
-  } catch (error) {
-    console.error("user.js backup error:", error.message);
-    res.status(500).send("backup error", error.message);
-  }
+  //     const backupFileName = `test1_backup.json`;
+  //     const backupFilePath = path.join(__dirname, "backups", backupFileName);
+  //     fs.writeFileSync(backupFilePath, JSON.stringify(backupData, null, 2));
+  //     res.download(backupFilePath, backupFileName, () => {
+  //       // 다운로드 후 백업 파일 삭제
+  //       fs.unlinkSync(backupFilePath);
+  //     });
+  //   } else {
+  //     res.status(500).json({ message: "유저를 찾을 수 없습니다." });
+  //   }
+  // } catch (error) {
+  //   console.error("user.js backup error:", error.message);
+  //   res.status(500).send("backup error", error.message);
+  // }
 });
 
 module.exports = router;
